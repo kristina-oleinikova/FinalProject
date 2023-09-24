4924a2562c9474b613c77c5c4183843c93f8851f
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-09-24 17:36:21 +0400

    added updates

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -3,12 +3,16 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+@Test
 @CucumberOptions(
         features = {"src/test/resources/features"},
-        plugin = {"json:target/json/file.json", "html:target/site/cucumber-pretty.html"},
+        plugin = {"json:target/json/file.json",
+                "html:target/site/cucumber-pretty.html",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         glue = "stepDefs",
-        tags = "@"
+        tags = ""
 )
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 
