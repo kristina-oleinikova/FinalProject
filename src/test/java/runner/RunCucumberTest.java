cfd1697557eb730a5be291230aa570541bfe2fbe
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-10-02 21:52:25 +0400

    fix comments

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -3,7 +3,6 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
 @CucumberOptions(
         features = {"src/test/resources/features"},
