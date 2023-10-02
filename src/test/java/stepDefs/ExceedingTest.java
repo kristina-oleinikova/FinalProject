b5ebbac8c9ae27e52600937654ca333d61e9ae11
kristina-oleinikova <kristina.oleinikova95@gmail.com>
2023-10-02 07:41:04 +0400

    added updates

--- src/test/java/stepDefs/ExceedingTest.java
+++ src/test/java/stepDefs/ExceedingTest.java
@@ -24,10 +24,12 @@ public class ExceedingTest extends BaseTest {
     public ExceedingTest(BaseTest baseTest){
         this.baseTest = baseTest;
     }
+
     @Given("Login Page open")
     public void loginPageOpen() {
         driver.get(ReadProperties.getUrl());
     }
+
     @When("enter Email {} and Password {}")
     public void enterEmailAndPassword(String email, String password) {
         loginPage = new LoginPage(driver);
@@ -35,28 +37,33 @@ public class ExceedingTest extends BaseTest {
         loginPage.passwordInput.sendKeys(password);
         logger.info("Login page is opened");
     }
+
     @And("User click loginButton")
     public void clickButtonProject(){
         loginPage.loginButton.click();
         logger.info("Projects page is opened");
     }
+
     @And("User click link project")
     public void clickLinkProject(){
         projectsPage = new ProjectsDashboardPage(driver);
         projectsPage.linkProject.click();
         logger.info("Project is opened");
     }
+
     @And("User click button Milestones")
     public void clickButtonMilestones(){
         projectPage = new ProjectPage(driver);
         projectPage.buttonMilestones.click();
     }
+
     @And("User click button add milestone")
     public void clickButtonAddMilestone(){
         milestonesPage = new MilestonesPage(driver);
         milestonesPage.addButton.click();
         logger.info("Milestone Page is opened");
     }
+
     @And("User create milestone")
     public void createMilestone(){
         milestonesPage.inputNameMilestone.sendKeys("Test");
@@ -64,6 +71,7 @@ public class ExceedingTest extends BaseTest {
         milestonesPage.panelStartDate.sendKeys("160920230");
         milestonesPage.addMilestoneButton.click();
     }
+
     @Then("Message {} is displayed")
     public void milestoneCreate(String error) {
         Assert.assertEquals(error,"The start date does not match the format Y-m-d.");
