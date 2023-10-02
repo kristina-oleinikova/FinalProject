ac159606cee5eeb65a656e491addd8d2e3db6705
Aleksei1364 <sapach1364@gmail.com>
2023-10-02 09:39:56 +0300

    fix model Run

--- src/test/java/models/Run.java
+++ src/test/java/models/Run.java
@@ -3,7 +3,7 @@ package models;
 import com.google.gson.annotations.SerializedName;
 import lombok.*;
 
-@Data
+@Builder
 public class Run {
     @SerializedName(value = "name")
     private String runName;
